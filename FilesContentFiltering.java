8c4f6c4fc90d863a3a55e3feec6f8cdc920386ce
sevaK5 <vsevolod.kristalev@gmail.com>
2024-08-08 13:43:12 +0800

    remove Math imoprt

--- FilesContentFiltering.java
+++ FilesContentFiltering.java
@@ -2,7 +2,6 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.io.*;
 import java.util.Scanner;
-import java.util.Math;
 
 class FilesContentFiltering {
     public static void main(String[] args) throws IOException {
