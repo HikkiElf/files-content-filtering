9538db664e3738c838700e4a7753f41f4b951796
sevaK5 <vsevolod.kristalev@gmail.com>
2024-08-08 14:40:42 +0800

    add showStats method, show only short statistics

--- FilesContentFiltering.java
+++ FilesContentFiltering.java
@@ -2,28 +2,36 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.io.*;
 import java.util.Scanner;
+import java.util.HashMap;
 
 class FilesContentFiltering {
+    private static HashMap<String, Integer> stats = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
+        boolean isShowStats = false;
         boolean appendFlag = false;
         String path = "";
         String prefix = "";
         Pattern patternIsTxt = Pattern.compile("^.*\\.(txt)$");
         for(int i = 0; i < args.length; i++) {
             Matcher matcherIsTxt = patternIsTxt.matcher(args[i]);
+            if(args[i].equals("-s")) {
+                // System.out.println("SHORT STATS ACTIVE");
+                isShowStats = true;
+            }
             if (args[i].equals("-a")) {
-                System.out.println("APPEND FLAG ACTIVE");
+                // System.out.println("APPEND ACTIVE");
                 appendFlag = true;
             }
             if (args[i].equals("-o")) {
-                System.out.println("SELECT OUTPUT PATH ACTIVE");
+                // System.out.println("SELECT OUTPUT PATH ACTIVE");
                 if (i+1 >= args.length)
                     return;
                 path = args[i+1] + "/";
                 i++;
             }
             if (args[i].equals("-p")) {
-                System.out.println("PREFIX ACTIVE");
+                // System.out.println("PREFIX ACTIVE");
                 if (i+1 >= args.length)
                     return;
                 prefix = args[i+1];
@@ -35,6 +43,18 @@ class FilesContentFiltering {
                 appendFlag = true;
           }
        }
+       if(isShowStats == true) {
+           showStats(false);
+       }
+    }
+    public static void showStats(boolean isFull) {
+        if (isFull == false) {
+            for (HashMap.Entry<String, Integer> entry: stats.entrySet()) {
+                String key = entry.getKey();
+                String value = Integer.toString(entry.getValue());
+                System.out.println(key + " " + value + " lines saved");
+            }
+        }
     }
 
     public static void contentFilter(String filename, boolean appendFlag, String path, String prefix) throws IOException {
@@ -50,18 +70,21 @@ class FilesContentFiltering {
                     try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path + prefix + "integers.txt", appendFlagInt)))) {
                         pw.println(s.next());
                         appendFlagInt = true;
+                        stats.merge("integers", 1, Integer::sum);
                     }
                 }
                 else if (s.hasNextDouble()) {
                     try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path + prefix + "floats.txt", appendFlagFloat)))) {
                         pw.println(s.next());
                         appendFlagFloat = true;
+                        stats.merge("floats", 1, Integer::sum);
                     }
                 }
                 else {
                     try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path + prefix + "strings.txt", appendFlagString)))) {
                         pw.println(s.next());
                         appendFlagString = true;
+                        stats.merge("strings", 1, Integer::sum);
                     }
                 }
             }
