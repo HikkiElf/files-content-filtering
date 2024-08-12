80a77078cceefa4eaa017d90017702433f0fa34c
sevaK5 <vsevolod.kristalev@gmail.com>
2024-08-12 11:51:33 +0800

    restructure project and add Allert class

---
+++ src/main/com/hikkielf/Allert.java
@@ -0,0 +1,15 @@
+package com.hikkielf;
+import com.hikkielf.Color;
+
+public class Allert {
+
+    
+
+    public static void warning(String message) {
+        System.out.println(Color.colorize(Color.yellow, "ВНИМАНИЕ ") + message);
+    }
+
+    public static void error(String message) {
+        System.out.println(Color.colorize(Color.red, "ОШИБКА ") + message);
+    }
+}
---
+++ src/main/com/hikkielf/Color.java
@@ -0,0 +1,26 @@
+package com.hikkielf;
+
+public enum Color {
+        
+    ANSI_RESET ("\u001B[0m"),
+    red ("\u001B[31m"),    
+    green ("\u001B[32m"),
+    yellow ("\u001B[33m"),    
+    blue ("\u001B[34m"),       
+    purple ("\u001B[35m"),    
+    cyan ("\u001B[36m"),      
+    white ("\u001B[37m");
+
+    private final String code;
+
+    Color(String code) {
+        this.code = code;
+    }
+
+    public static String colorize(Color color, Object message) {
+        return color.code + message + Color.ANSI_RESET.code;
+    }
+    private String colorize(Object message) {
+        return this.code + message + Color.ANSI_RESET.code;
+    }
+}
